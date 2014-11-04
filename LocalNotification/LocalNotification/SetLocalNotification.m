05e4487d92c56f64d629f914127de8b9e4b59cb8
iOSBeginner <jenhausu@icloud.com>
2014-11-04 22:23:25 +0800

    Local Notification設定

--- LocalNotification/LocalNotification/SetLocalNotification.m
+++ LocalNotification/LocalNotification/SetLocalNotification.m
@@ -7,7 +7,27 @@
 //
 
 #import "SetLocalNotification.h"
+#import <UIKit/UIKit.h>
 
 @implementation SetLocalNotification
+
+- (void)setLocalNotificationWithMessage:(NSString *)message fireDate:(NSDate *)fireDate repeatOrNot:(BOOL)repeat Sound:(NSString *)sound key:(NSString *)key forValue:(id)value
+{
+    UILocalNotification *localNotification = [[UILocalNotification alloc] init];
+    localNotification.fireDate = fireDate;
+    localNotification.alertBody = message;
+    
+    localNotification.timeZone = [NSTimeZone defaultTimeZone];
+    localNotification.soundName = sound;
+    
+    if (repeat) localNotification.repeatInterval = NSCalendarUnitDay;
+    
+    if (key) {
+        NSDictionary *dic = [NSDictionary dictionaryWithObject:value forKey:key];
+        localNotification.userInfo = dic;
+    }
+    
+    [[UIApplication sharedApplication] scheduleLocalNotification:localNotification];
+}
 
 @end
