a23225792d30d70adb9aa92d0540441bb251d567
iOSBeginner <jenhausu@icloud.com>
2014-11-04 20:20:19 +0800

    initialization

---
+++ LocalNotification/LocalNotification/SetLocalNotification.h
@@ -0,0 +1,13 @@
+//
+//  SetLocalNotification.h
+//  LocalNotification
+//
+//  Created by 蘇健豪1 on 2014/11/4.
+//  Copyright (c) 2014年 蘇健豪. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SetLocalNotification : NSObject
+
+@end
---
+++ LocalNotification/LocalNotification/SetLocalNotification.m
@@ -0,0 +1,13 @@
+//
+//  SetLocalNotification.m
+//  LocalNotification
+//
+//  Created by 蘇健豪1 on 2014/11/4.
+//  Copyright (c) 2014年 蘇健豪. All rights reserved.
+//
+
+#import "SetLocalNotification.h"
+
+@implementation SetLocalNotification
+
+@end
